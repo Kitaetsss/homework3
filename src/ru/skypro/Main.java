2a88f41758f40fff4efdef7c4037dc5ee15afde6
Kitaetsss <kitayets1@mail.ru>
2021-12-31 15:47:03 +0500

    homework3

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,26 @@
+package ru.skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int ios = 1;
+       if (ios <= 1) {
+        System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
+       if (ios >=10) {
+           System.out.println("Установите версию приложения для android по ссылке");
+    }
+        int android = 10;
+        if (android <= 1) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
+        if (android >=10) {
+            System.out.println("Установите версию приложения для android по ссылке");
+        }
+        System.out.println("Задание номер 2");
+
+
+    }
+}
